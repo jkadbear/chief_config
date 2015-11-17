fb61a0df8e5cbbb0309ac6534b8b0a621f07c313
jkadbear <jkadbear@gmail.com>
2015-11-17 18:49:50 +0000

    remove '.' before filename

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cp ~/.zshrc .
-cp ~/.vimperatorrc .
-cp ~/.vimrc .
-cp ~/.tmux.conf .
+cp ~/.zshrc ./zshrc
+cp ~/.vimperatorrc ./vimperatorrc
+cp ~/.vimrc ./vimrc
+cp ~/.Xmodmap ./Xmodmap
+cp ~/.tmux.conf ./tmux.conf
