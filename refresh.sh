01ba374ef1056e65aeab212cc1922a4fd4b766da
jkadbear <jkadbear@gmail.com>
2015-11-17 21:08:40 +0000

    add proxychains.conf

--- refresh.sh
+++ refresh.sh
@@ -3,4 +3,5 @@ cp ~/.zshrc ./zshrc
 cp ~/.vimperatorrc ./vimperatorrc
 cp ~/.vimrc ./vimrc
 cp ~/.Xmodmap ./Xmodmap
+cp ~/.proxychains/proxychains.conf ./proxychains.conf
 cp ~/.tmux.conf ./tmux.conf
